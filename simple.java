582e6c06912c4e714b3d3ded2bb9bd3e78a7f682
Sai Kartik <saikartikkappagantula@gmail.com>
2024-11-07 13:50:53 +0530

    changes made

--- simple.java
+++ simple.java
@@ -1,15 +1,6 @@
 public class simple {
-
-  public static void main(String args[]) {
-    long nFactoriral = factorialProgram(9);
-    System.out.println(nFactoriral);
-  }
-  /* Java factorial program with recursion. */
-  public static long factorialProgram(long n) {
-    if (n <= 1) {
-      return 1;
-    } else {
-      return n * factorialProgram(n - 1);
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
-  }
 }
+
